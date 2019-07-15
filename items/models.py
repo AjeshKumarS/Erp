d4eaddfe802f1032c596b5141625f10349641a26
Sneha Saj <snehasaj1999@gmail.com>
2019-07-15 09:11:45 +0530

    models added

--- items/models.py
+++ items/models.py
@@ -1,13 +1,38 @@
 from django.db import models
-
-
-class Item(models.Model):
-	pass
+from django.utils import timezone
 
 
 class Uom(models.Model):
-	pass
+    uom = models.CharField(max_length = 50 ,unique = True) 
+    
+    def __str__(self):
+        return self.uom
 
 
-class ItemUom(models.Model):
-	pass
+
+class Item(models.Model):
+    item_code = models.IntegerField(primary_key = True)
+    item_desc = models.CharField(max_length = 300)
+    price = models.IntegerField()
+    uom = models.ForeignKey(Uom, on_delete=models.CASCADE)
+   
+    
+    
+    def __str__(self):
+        return self.item_desc
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+    
+
